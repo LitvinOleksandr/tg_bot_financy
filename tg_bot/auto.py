0a482beb3c41f90ceeeb1d8d8d66fe53590b7203
OleksandrLitvin <sanek.litvin@gmail.com>
2025-03-05 23:32:30 +0100

    some changes

--- tg_bot/auto.py
+++ tg_bot/auto.py
@@ -1,16 +1,1 @@
-import sqlite3
-import datetime
-from datetime import datetime, timedelta, timezone
-from functools import wraps
-
-
-def with_db_connection(db_path='../db.sqlite3'):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            with sqlite3.connect(db_path) as conn:
-                cursor = conn.cursor()
-                result = func(cursor, *args, **kwargs)
-                return result
-        return wrapper
-    return decorator
+from simple_function import with_db_connection
--- tg_bot/help.py
+++ tg_bot/help.py
@@ -3,7 +3,8 @@ def help_message(message: str) -> str:
     if len(message) < 2:
         return "Help <name>\nname: add, auto, poket, shop."
     if message[1] == "add":
-        return (f"'add <shop_name> <amount>' - оплата картой.\n'add <shop_name> <amount> cash' - оплата наличными."
+        return (f"'add <shop_name> <amount>' - оплата картой."
+                f"\n'add <shop_name> <amount> cash' - оплата наличными."
                 f"\nНапример: add lidl 12,45. Разделение . или , не имеет значения.")
     if message[1] == "auto":
         return "Help auto"
